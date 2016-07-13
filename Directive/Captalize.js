822dff4cc6174a1b638ee4289ad090969203c6b9
Parveen Sachdeva <sachdeva176@gmail.com>
2016-07-13 16:27:55 +0530

    Add files via upload

---
+++ Directive/Captalize.js
@@ -0,0 +1,21 @@
+﻿app.directive('capitalize', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, modelCtrl) {
+            var capitalize = function (inputValue) {
+                console.log(inputValue);
+                if (inputValue == undefined) inputValue = "";
+                var capitalized = inputValue.charAt(0).toUpperCase() + inputValue.substring(1);
+                if (capitalized !== inputValue) {
+                    modelCtrl.$setViewValue(capitalized);
+                    modelCtrl.$render();
+                }
+                return capitalized;
+            };
+            modelCtrl.$parsers.push(capitalize);
+            capitalize(scope[attrs.ngModel]);
+        }
+    };
+});
+
+
